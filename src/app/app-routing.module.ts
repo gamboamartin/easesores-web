c72477a3d5413f2933a603652363a05f8d865a01
Angel Martínez De León <angelmartinezdeleon@gmail.com>
2022-07-27 13:49:49 -0500

    Se organizaron los estilos y se crearon los modulos de hogar, header y compra

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CompraComponent} from "./components/compra/compra.component";
+import {HogarComponent} from "./components/hogar/hogar.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HogarComponent
+  },
+  {
+    path: 'compra',
+    component: CompraComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CompraComponent } from './components/compra/compra.component';
+import { HogarComponent } from './components/hogar/hogar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CompraComponent,
+    HogarComponent
   ],
   imports: [
     BrowserModule,
