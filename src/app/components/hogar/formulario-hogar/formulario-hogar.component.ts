f6254ebe9656328f787b8d12bd21da19811b408a
Angel Martínez De León <angelmartinezdeleon@gmail.com>
2022-08-03 12:47:14 -0500

    Se crea componente formulario de la vista hogar

---
+++ src/app/components/hogar/formulario-hogar/formulario-hogar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-hogar',
+  templateUrl: './formulario-hogar.component.html',
+  styleUrls: ['./formulario-hogar.component.css']
+})
+export class FormularioHogarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
