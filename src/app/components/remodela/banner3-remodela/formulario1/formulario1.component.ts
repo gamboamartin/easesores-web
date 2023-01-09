e9790d7fb96d671e5bea192d2c9c75afed0ee46b
[user] <angel.martinez@ivitec.com.mx>
2023-01-09 14:44:05 -0600

    Se agrega disabled en formularios

--- src/app/components/remodela/banner3-remodela/formulario1/formulario1.component.ts
+++ src/app/components/remodela/banner3-remodela/formulario1/formulario1.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-formulario1',
   templateUrl: './formulario1.component.html',
   styleUrls: ['./formulario1.component.css']
 })
 export class Formulario1Component implements OnInit {
+  nextForm(){
+    let form2 = document.getElementById("form2");
+    let form3 = document.getElementById("form3");
+    // @ts-ignore
+    if (form2.style.display != "block"){
+      // @ts-ignore
+      form2.style.display = "block"
+      // @ts-ignore
+      form3.style.display = "block"
+    }
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
