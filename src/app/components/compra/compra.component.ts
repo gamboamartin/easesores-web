772188d484c08ed32e81971282f23b21a1a04774
[user] <angel.martinez@ivitec.com.mx>
2023-02-16 14:45:10 -0600

    Se integra animacion buscador en compra

--- src/app/components/compra/compra.component.ts
+++ src/app/components/compra/compra.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compra.component.css']
 })
 export class CompraComponent implements OnInit {
-
+  focusAnimation() {
+    // @ts-ignore
+    document.getElementById('div1').style.animation = "margin 0.2s ease-in-out forwards";
+    // @ts-ignore
+    document.getElementById('searchbox').style.animation = "desplazamiento 0.5s ease-in-out forwards";
+  }
+  blurAnimation() {
+    // @ts-ignore
+    document.getElementById('div1').style.animation = "marginEvil 0.4s ease-in-out forwards";
+    // @ts-ignore
+    document.getElementById('searchbox').style.animation = "desplazamientoEvil 0.4s ease-in-out forwards";
+  }
   constructor() { }
 
   ngOnInit(): void {
